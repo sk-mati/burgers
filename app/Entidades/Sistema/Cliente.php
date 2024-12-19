c1455452d75db7987a6b5b8d2bbd4aa04a0051a6
sk-mati <mati.skmj@gmail.com>
2024-12-19 13:51:49 -0300

    cambios_19_12

---
+++ app/Entidades/Sistema/Cliente.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Entidades;
+
+use DB;//Es como el mysqli anteriormente utilizado
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+
+    protected $table = 'clientes';
+    public $timestamps = false;//Si es true, inserta una marca en la base de datos con fecha y hora de inserción
+
+    protected $fillable = [ //Son los campos de la tabla clientes en la BBDD
+        'idcliente', 'nombre', 'apellido', 'correo', 'dni', 'celular', 'clave',
+    ];
+
+    protected $hidden = [
+
+    ];
+
+    public function obtenerTodos() //Método
+    {
+        //Arma la query
+        $sql = "SELECT 
+                  idcliente,
+                  nombre,
+                  telefono,
+                  direccion,
+                  dni,
+                  correo,
+                  clave
+                FROM clientes ORDER BY nombre";
+        //Ejecuta la query
+        $lstRetorno = DB::select($sql); //Método static. Permite llamar sin instanciar. Hace todo el fetch_assoc en una línea.
+        return $lstRetorno; //Devuelve array con datos.
+    }
+
+    public function obtenerPorId($idCliente)
+    {
+        $sql = "SELECT
+                    idcliente,
+                    nombre,
+                    telefono,
+                    direccion,
+                    dni,
+                    correo,
+                    clave
+                FROM clientes WHERE idcliente = $idCliente";
+        $lstRetorno = DB::select($sql);
+
+        if (count($lstRetorno) > 0) {
+            $this->idcliente = $lstRetorno[0]->idcliente;
+            $this->nombre = $lstRetorno[0]->nombre;
+            $this->telefono = $lstRetorno[0]->telefono;
+            $this->direccion = $lstRetorno[0]->direccion;
+            $this->dni = $lstRetorno[0]->dni;
+            $this->correo = $lstRetorno[0]->correo;
+            $this->clave = $lstRetorno[0]->clave;
+            return $this;
+        }
+        return null;
+    }
+
+    public function guardar() {
+        $sql = "UPDATE clientes SET
+            nombre='$this->nombre',
+            telefono='$this->telefono',
+            direccion='$this->direccion',
+            dni='$this->dni',
+            correo='$this->correo',
+            clave='$this->clave'
+            WHERE idcliente=?"; //El signo de interrogación indica que lo busca en el parámetro (más seguro). Filtro de inyeccioón SQL.
+        $affected = DB::update($sql, [$this->idcliente]); //Arma la query por parámetro
+    }
+
+    public function eliminar()
+    {
+        $sql = "DELETE FROM clientes WHERE
+            idcliente=?";
+        $affected = DB::delete($sql, [$this->idcliente]);
+    }
+
+    public function insertar()
+    {
+        $sql = "INSERT INTO clientes (
+                    nombre,
+                    telefono,
+                    direccion,
+                    dni,
+                    correo,
+                    clave
+            ) VALUES (?, ?, ?, ?, ?, ?);";
+        $result = DB::insert($sql, [
+            $this->nombre,
+            $this->telefono,
+            $this->direccion,
+            $this->dni,
+            $this->correo,
+            $this->clave,
+        ]);
+        return $this->idcliente = DB::getPdo()->lastInsertId();
+    }
+
+}
+
+?>
---
+++ app/Entidades/Sistema/Producto.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entidades;
+
+use DB;
+use Illuminate\Database\Eloquent\Model;
+
+class Producto extends Model
+{
+
+    protected $table = 'productos';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'idproducto', 'nombre', 'cantidad', 'precio', 'imagen',
+    ];
+
+    protected $hidden = [
+
+    ];
+
+    public function obtenerTodos()
+    {
+        $sql = "SELECT 
+                  idproducto,
+                  nombre,
+                  cantidad,
+                  precio,
+                  imagen
+                FROM productos ORDER BY nombre";
+        $lstRetorno = DB::select($sql);
+        return $lstRetorno;
+    }
+
+    public function obtenerPorId($idProducto)
+    {
+        $sql = "SELECT
+                  idproducto,
+                  nombre,
+                  cantidad,
+                  precio,
+                  imagen
+                FROM productos WHERE idproducto = $idProducto";
+        $lstRetorno = DB::select($sql);
+
+        if (count($lstRetorno) > 0) {
+            $this->idproducto = $lstRetorno[0]->idproducto;
+            $this->nombre = $lstRetorno[0]->nombre;
+            $this->cantidad = $lstRetorno[0]->cantidad;
+            $this->precio = $lstRetorno[0]->precio;
+            $this->imagen = $lstRetorno[0]->imagen;
+            return $this;
+        }
+        return null;
+    }
+
+    public function guardar() {
+        $sql = "UPDATE productos SET
+            nombre='$this->nombre',
+            cantidad='$this->cantidad',
+            precio='$this->precio',
+            imagen='$this->imagen',
+            WHERE idproducto=?"; 
+        $affected = DB::update($sql, [$this->idproducto]);
+    }
+
+    public function eliminar()
+    {
+        $sql = "DELETE FROM productos WHERE
+            idproducto=?";
+        $affected = DB::delete($sql, [$this->idproducto]);
+    }
+
+    public function insertar()
+    {
+        $sql = "INSERT INTO clientes (
+                    nombre,
+                    cantidad,
+                    precio,
+                    imagen
+            ) VALUES (?, ?, ?, ?);";
+        $result = DB::insert($sql, [
+            $this->nombre,
+            $this->cantidad,
+            $this->precio,
+            $this->imagen,
+        ]);
+        return $this->idproducto = DB::getPdo()->lastInsertId();
+    }
+
+}
+
+?>
