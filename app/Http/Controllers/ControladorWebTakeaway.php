b20aeffc1b235b9603c56929dc2c1eb094835d3d
sk-mati <mati.skmj@gmail.com>
2025-02-08 17:49:38 -0300

    changes_08_02

--- app/Http/Controllers/ControladorWebTakeaway.php
+++ app/Http/Controllers/ControladorWebTakeaway.php
@@ -5,6 +5,12 @@ namespace App\Http\Controllers;
 use App\Entidades\Producto;
 use App\Entidades\Sucursal;
 use App\Entidades\Categoria;
+use App\Entidades\Carrito;
+use Illuminate\Http\Request;
+
+require app_path().'/start/constants.php';
+
+use Session;
 
 class ControladorWebTakeaway extends Controller
 {
@@ -19,11 +25,59 @@ class ControladorWebTakeaway extends Controller
         $categoria = new Categoria();
         $aCategorias = $categoria->obtenerTodos();
 
-        return view("web.takeaway", compact('sucursal', 'aSucursales', 'producto', 'aProductos', 'categoria', 'aCategorias'));
+        return view("web.takeaway", compact('aSucursales', 'aProductos', 'aCategorias'));
     }
 
-    public function insertar()
+    public function insertar(Request $request)
     {
-        
+        $idCliente = Session::get("idCliente");
+
+        $idProducto = $request->input("txtProducto");
+        $cantidad = $request->input("txtCantidad");
+
+        if(isset($idCliente) && $idCliente > 0) {
+
+            $sucursal = new Sucursal();
+            $aSucursales = $sucursal->obtenerTodos();
+
+            $producto = new Producto();
+            $aProductos = $producto->obtenerTodos();
+
+            $categoria = new Categoria();
+            $aCategorias = $categoria->obtenerTodos();
+
+            if(isset($cantidad) && $cantidad > 0) {
+
+                $carrito = new Carrito();
+                $carrito->fk_idcliente = $idCliente;
+                $carrito->fk_idproducto = $idProducto;
+                $carrito->cantidad = $cantidad;
+                $carrito->insertar();
+
+                $msg["ESTADO"] = MSG_SUCCESS;
+                $msg["MSG"] = "El producto se ha agregado al carrito.";
+                return view('web.takeaway', compact('aSucursales', 'aProductos', 'aCategorias', 'msg'));
+            } else {
+
+                $msg["ESTADO"] = MSG_ERROR;
+                $msg["MSG"] = "No agregó ningún producto al carrito.";
+                return view('web.takeaway', compact('aSucursales', 'aProductos', 'aCategorias', 'msg'));
+            }
+
+        } else {
+
+            $sucursal = new Sucursal();
+            $aSucursales = $sucursal->obtenerTodos();
+
+            $producto = new Producto();
+            $aProductos = $producto->obtenerTodos();
+
+            $categoria = new Categoria();
+            $aCategorias = $categoria->obtenerTodos();
+
+            $msg["ESTADO"] = MSG_ERROR;
+            $msg["MSG"] = "Debe iniciar sesión para realizar un pedido.";
+            return view('web.takeaway', compact('aSucursales', 'aProductos', 'aCategorias', 'msg')); 
+        }
     }
 }
--- resources/views/web/takeaway.blade.php
+++ resources/views/web/takeaway.blade.php
@@ -1,13 +1,22 @@
 @extends("web.plantilla")
 @section("contenido")
   <!-- food section -->
-
   <section class="food_section layout_padding">
     <div class="container">
       <div class="heading_container heading_center">
         <h2>
           Nuestro Menú
         </h2>
+        <br>
+        @if(isset($msg))
+        <div class="row">
+          <div class="col-md-12">
+            <div class="alert alert-{{ $msg['ESTADO'] }}" role="alert">
+              {{ $msg["MSG"] }}
+            </div>
+          </div>
+        </div>
+        @endif
       </div>
 
       <ul class="filters_menu">
@@ -35,13 +44,26 @@
               </p>
               <div class="options">
                 <h6>
-                  $ {{ $producto->precio }}
+                  $ {{ number_format($producto->precio, 0, ",", ".") }}
                 </h6>
-                <a href="/carrito" class="cart_link">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
-                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
-                  </svg>
-                </a>
+                <form action="" method="POST">
+                  <div class="row">
+                    <div class="col-6">
+                      <input type="hidden" name="_token" value="{{ csrf_token() }}"></input>
+                      <input type="hidden" id="txtProducto" name="txtProducto" class="form-control" style="width: 125px;" min="1" value="{{ $producto->idproducto }}" required>
+                      <input type="number" id="txtCantidad" name="txtCantidad" class="form-control" style="width: 125px;" placeholder="Cantidad" min="1" value="">
+                    </div>
+                    <div class="col-6">
+                      <button type="submit" class="btn btn-carrito">
+                        <a href="" class="cart_link">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
+                            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+                          </svg>
+                        </a>
+                      </button>
+                    </div>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
