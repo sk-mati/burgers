cfe9325069983481789480a2a052d430d1dd1142
sk-mati <mati.skmj@gmail.com>
2025-02-21 11:18:10 -0300

    changes

--- resources/views/web/nosotros.blade.php
+++ resources/views/web/nosotros.blade.php
@@ -20,9 +20,56 @@
               Somos Burguers SRL, dedicados a la gastronomía de excelencia
             </h5>
           </div>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta iste sapiente officiis cumque voluptate exercitationem fugit, illo ipsum esse rem enim facere ad ipsa. Amet aspernatur odio error architecto veniam?
-          </p>
+          <div id="customCarousel1" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <div class="container ">
+                  <div class="row">
+                    <div class="col-md-12 col-lg-8">
+                      <div class="detail-box">
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum nisi consequuntur est numquam enim dignissimos voluptate eveniet laborum suscipit officia, soluta tempora cumque earum rem vitae sint obcaecati dicta eos?
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="carousel-item ">
+                <div class="container ">
+                  <div class="row">
+                    <div class="col-md-12 col-lg-8">
+                      <div class="detail-box">
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore impedit quas neque? Error quidem assumenda magni? Perspiciatis veritatis quis sit, explicabo accusantium perferendis ullam voluptatum tenetur neque, sint ea tempora?
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="container ">
+                  <div class="row">
+                    <div class="col-md-12 col-lg-8 ">
+                      <div class="detail-box">
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, deleniti quasi quo suscipit eum, velit fugiat vitae illo repellat ipsam doloribus eaque harum autem non, minima quis nam mollitia necessitatibus!
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="container py-3">
+              <ol class="carousel-indicators">
+                <li data-target="#customCarousel1" data-slide-to="0" class="active"></li>
+                <li data-target="#customCarousel1" data-slide-to="1"></li>
+                <li data-target="#customCarousel1" data-slide-to="2"></li>
+              </ol>
+            </div>
+          </div>
           <a href="#trabaja">
             Trabajá con nosotros
           </a>
